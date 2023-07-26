49ca911ddca023bf45d38a9a6f648323d8c1c8da
An10nella <135998311+An10nella@users.noreply.github.com>
2023-07-26 20:21:27 +0300

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+const accounts = [["admin01", "123"] , ["admin02","234"] , ["admin03", "345"] ]
 const loginBtn = document.getElementById("login")
 const usernameEl = document.getElementById("username")
 const passwordEl = document.getElementById("password")
@@ -7,5 +8,15 @@ document.title = ("LogIn Page")
 loginBtn.addEventListener("click",function(){
     let accessUser = usernameEl.value
     let accessPass = passwordEl.value
+    for (let i = 0; i<accounts.length; i++){
+        console.log(accounts[i][0])
+        // for(let j=0; j<=i.length; j++){
+        //     console.log(i[j])
+        // } 
+        if (accessUser == accounts[i][0] && accessPass == accounts[i][1]){
+            console.log('welcome ${accessUser}')
+        }
+    }
     // console.log(accessUser , accessPass)
+
 })
