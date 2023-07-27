c8d889ac79c2a21be76765b49483c1a2a701476a
An10nella <135998311+An10nella@users.noreply.github.com>
2023-07-27 20:21:32 +0300

    limit the attempt login

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,11 @@ const usernameEl = document.getElementById("username")
 const passwordEl = document.getElementById("password")
 
 document.title = ("LogIn Page")
+let counter = 1
 
 loginBtn.addEventListener("click",function(){
+    if(counter < 4){
+
     let accessUser = usernameEl.value
     let accessPass = passwordEl.value
     let found = false
@@ -16,19 +19,25 @@ loginBtn.addEventListener("click",function(){
             if(accessUser == accounts[i][0] && accessPass == accounts[i][1] ){
             // alert(`welcome ${accessUser}`)
             found = true
+            counter = 1
             document.write(`<h1 style="text-align:center; color:blue; margin-top:50px; ">
             welcome <span style= "font-weight:bold; color:red;"> ${accessUser.toUpperCase()}
             </span></h1>`)
             // setTimeout(() => location.href='login.html' , 2000);
-            setTimeout(secondPage, 2000);
+            setTimeout(secondPage, 2000)
             
             
-
         }
     }
     if (found == false){
-        alert(`Invalid Username or Password`)
+        counter ++
+        alert(`Invalid Username or Password.\n
+        You still have ${4-counter} attempts `)
     }
+}else{
+    alert("Maximum Attempts Exceeded!")
+
+}
     
       
     
